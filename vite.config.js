e0f9cf837897791188667f91dacf848173cda067
Dmitriy Konnov <dkonnov@131.ru>
2026-01-04 19:54:02 +0300

    DS version 2

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,28 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/birthday-invitation/',
+  base: process.env.NODE_ENV === 'production' ? '/birthday-invitation/' : '/',
   server: {
     port: 3000
-  }
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  esbuild: {
+    loader: 'jsx',
+    include: /src\/.*\.jsx?$/,
+    exclude: [],
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx',
+      },
+    },
+  },
 })
